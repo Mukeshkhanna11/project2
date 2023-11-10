e5c8830106932fff3a79a89d2183cb859e2e416c
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-11-10 15:17:59 +0530

    new project2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [inputValue, setinputValue] = useState("");
+  const [colorChange, setColorChange] = useState("black");
+  const [count, setCount] = useState(0);
+  const handleChange = () => {
+    setAge(age + 1);
+  };
+  const handleOnChange = (event) => {
+    setinputValue(event.target.value);
+  };
+  const showOrHide = () => {
+    setColorChange(colorChange === "black" ? "red" : "black");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {age}
+      <div>
+        <button onClick={handleChange}>click here</button>
+      </div>
+
+      <input type="text" onChange={handleOnChange} />
+      <div>{inputValue}</div>
+      <div>
+        <button onClick={showOrHide}>SHOW/HIDE</button>
+        {<h1 style={{ color: colorChange }}>HI MY NAME IS MUKESH</h1>}
+      </div>
+
+      <div>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          DECREASE
+        </button>
+        <div>{count}</div>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          INCREASE
+        </button>
+        <div>
+          <button
+            onClick={() => {
+              setCount(0);
+            }}
+          >
+            SET TO ZERO
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
